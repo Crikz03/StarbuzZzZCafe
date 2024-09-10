dfb80152427c5593df1badbb07f6799251e5b97c
CriCrin03 <chris012203@gmail.com>
2024-09-10 01:20:14 -0700

    ultimo push

--- StarbuzzCoffe_240005/src/Dominio/Expresso.java
+++ StarbuzzCoffe_240005/src/Dominio/Expresso.java
@@ -8,6 +8,14 @@ package Dominio;
  *
  * @author Chris
  */
-public class Expresso {
-    
+public class Expresso extends Beverage{
+
+    /**
+     * Constructor for Expresso. Initializes the description and cost for
+     * espresso.
+     */
+    public Expresso() {
+        this.description = "Expresso\nExtra ingredients:\n";
+        this.cost = 40;
+    }
 }
--- StarbuzzCoffe_240005/src/Dominio/HouseBlend.java
+++ StarbuzzCoffe_240005/src/Dominio/HouseBlend.java
@@ -8,6 +8,14 @@ package Dominio;
  *
  * @author Chris
  */
-public class HouseBlend {
-    
+public class HouseBlend extends Beverage {
+
+    /**
+     * Constructor for HouseBlend. Initializes the description and cost for a
+     * house blend coffee.
+     */
+    public HouseBlend() {
+        this.description = "HouseBlend\nExtra ingredients:";
+        this.cost = 50;
+    }
 }
